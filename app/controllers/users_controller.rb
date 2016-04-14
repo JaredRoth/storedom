d231c7e4dff4833611be14324e0439b3c59db25c
Jared Roth <jared.roth@gmail.com>
2016-04-14 11:18:35 -0600

    Create initial repo
    Initial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Successfully Created a User"
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:error] = @user.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit( :name,
+                                  :email,
+                                  :username,
+                                  :password,
+                                  :password_confirmation)
+  end
 end
